d85077bfb7fab539a34e5994ac54f02f7046b0f4
queen <queennjoki285@gmail.com>
2025-01-27 16:23:37 +0300

    validate input js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
 function usersInput(){
     const birthdate = document.getElementsById("birthdate").value;
     const gender = document.getElementById("select").value;
+
+    console.log(birthdate,gender)
+
+    if (day <= 0 || day > 31) {
+        alert("Please enter a valid day.");
+        return;
+    }
+
+    if (month <= 0 || month > 12) {
+        alert("Please enter a valid month.");
+        return;
+    }
 }
